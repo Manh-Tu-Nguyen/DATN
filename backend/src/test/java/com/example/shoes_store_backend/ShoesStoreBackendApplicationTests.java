b7180d4139f6d9e2e7ebe93a00c2c95ee5f262f7
Manh tu Nguyen <manhtunguyen1702@gmail.com>
2025-11-29 22:05:23 +0700

    Commit lan dau tien

---
+++ backend/src/test/java/com/example/shoes_store_backend/ShoesStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoes_store_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
